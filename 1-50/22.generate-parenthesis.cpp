b24c725ed7639644d81eceaea031a4783dff53bd
Chunxu Zou <471500439@qq.com>
2020-06-13 01:06:07 +0800

    generateParenthesis

---
+++ 1-50/22.generate-parenthesis.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+
+class Solution {
+public:
+    vector<string> res;
+    vector<string> generateParenthesis(int n) {
+        dfs(n,0,0,"");
+        return res;
+    }
+
+    void dfs(int n,int left,int right,string out) {
+        if(n == left && n == right) {
+            res.push_back(out);
+            return;
+        }
+
+        if (left < n) {
+            dfs(n,left+1,right,out+"(");
+        }
+        if(right < left) {
+            dfs(n,left,right+1,out+")");
+        }
+    }
+};
