e05bd18392312ae9c7f0d55556e173180b1eefe0
Chunxu Zou <471500439@qq.com>
2020-06-13 00:55:35 +0800

    levelOrder

---
+++ 100-150/102.level-order.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <queue>
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        if(root == nullptr) return {};
+        vector<vector<int>> res;
+        queue<TreeNode* > q;
+        q.push(root);
+        while (!q.empty()) {
+            vector<int> tr;
+            int tmpSize = q.size();
+            for (int i = 0; i < tmpSize; i++) {
+                TreeNode * t = q.front();
+                q.pop();
+                tr.push_back(t->val);
+                if (t->left) {
+                    q.push(t->left);
+                }
+                if (t->right) {
+                    q.push(t->right);
+                }
+            }
+            res.push_back(tr);
+        }
+        return res;
+    }
+};
