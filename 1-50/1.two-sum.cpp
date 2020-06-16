3151b581126f8164fcaa283402cbc4cdb3a63555
Chunxu Zou <471500439@qq.com>
2020-06-16 23:25:47 +0800

    two-sum

---
+++ 1-50/1.two-sum.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> m;
+        for (int i=0;i<nums.size();i++) {
+            int tmp = target - nums[i];
+            if(m.count(tmp)) {
+                return {m[tmp],i};
+            }
+            m[nums[i]] = i;
+        }
+        return {};
+    }
+};X
